0044ad9cecad1d8ed9089d91788b723e4c560c74
hammadmuntazir <hammadmuntazir58@gmail.com>
2025-02-05 12:14:50 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict";//treat all js code as newer version 
+
+//alert(3 + 3)//we are using node js,not browser
+
+console.log(3+3) //code readability should be high
+console.log("Hammad")
+let name ="hammad"
+let age =18
+let isLoggedIn=false
+
+//number =>2 to power of 53
+//bigInt for big integer 
+//string =>" in double or single quote"
+//boolean =>true/false
+//null =>stand alone value
+//undefined=>jub value define  nai ki ,null mean empty ,temp=null
+//symbol =>unique
+
+//object
+console.log(typeof age);//will tell me type of variable
+//type of undefined is undefined while null is object
