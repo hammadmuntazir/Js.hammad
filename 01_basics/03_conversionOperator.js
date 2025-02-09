3e3cc362be715b6ffc335c0fe46cb5b085899482
hammadmuntazir <hammadmuntazir58@gmail.com>
2025-02-09 14:40:37 +0500

    Create 03_conversionOperator.js

---
+++ 01_basics/03_conversionOperator.js
@@ -0,0 +1,29 @@
+// // //let score=33
+// // //let score = "33abc"//string
+// // //let score =null //0 result
+// // //let score =undefined//naN
+// // //let score=true//boolean varaible fale =0,true =1
+// // console.log(typeof(score));
+
+// // let valueInNumber = Number(score)
+// // console.log(typeof(valueInNumber));
+// // console.log(valueInNumber)
+// // //value ki type number mnn convert to ho jaye gi mgr jub print krvaye gyein to aye ga not a number
+// // //"33"=>33
+// // //"33abc"=>Nan
+// // //true=>1;false=>0
+
+// //let isLoggedIn= 1
+// //let isLoggedIn= ''//empty will return false
+// let isLoggedIn ="Hammad"//name bi true araha hai
+// let booleanIsLoggedIn =Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// //1 =>true ; 0 =>false 
+// //""=>false 
+// //"stringe with value" =>true
+
+let someNumber =33
+
+let stringnumber = String(someNumber)
+console.log(str)
