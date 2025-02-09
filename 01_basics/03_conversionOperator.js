f26f87ef324e524b151da8873d39224b3549f086
hammadmuntazir <hammadmuntazir58@gmail.com>
2025-02-09 10:47:35 +0000

    03_conversionOperators.js

--- 01_basics/03_conversionOperator.js
+++ 01_basics/03_conversionOperator.js
@@ -23,7 +23,57 @@
 // //""=>false 
 // //"stringe with value" =>true
 
-let someNumber =33
+// let someNumber =33
 
-let stringnumber = String(someNumber)
-console.log(str)
+// let stringnumber = String(someNumber)
+// console.log(stringnumber)
+// //********************************* Operations ***********************/
+// let value = 3
+// let negValue = -value
+// //console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1="Hello"
+// let str2 ="Hammad"
+
+// let str3=str1 +str2 //subtract nai hota just add hota hai
+// console.log(str3);
+
+// console.log("1"+2);//12
+// console.log(1+"2");//12
+// console.log("1"+"2");//12
+// console.log("1"+2+2);//122
+// console.log(1+2+"2")//32
+// //string first hai to subko string mn hi dekha jaye ga
+// console.log(true)//true 
+// console.log(+true)//1  while console.log(true+) is wrong
+// console.log(+"")//empty kry to zero atta hai + conversion krdeta hai boolean ki
+
+let num1 , num2, num3
+
+num1 = num2 = num3 =2+2
+
+let gameCounter =100
+gameCounter++;
+console.log(gameCounter)
+
+/*post and preincrement
+let x = 5;
+const y = x++;
+//first print then increase 
+console.log("x ="x,"y="y);
+Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+//first increase then print
+console.log("a ="a,"b"=b);
+// Expected output: "a:4, b:4"
+*/  
