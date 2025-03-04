34555293b47ef49955df4375aaf9f5838edfa5b8
hammadmuntazir <hammadmuntazir58@gmail.com>
2025-03-04 14:52:23 +0000

    02_arrays.js

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -1,7 +1,7 @@
 const marvel_heros =["Thor","Ironman","Spiderman"]
 const dc_heros =["Superman","Flash","Batman"]
                       //1st Method 
-marvel_heros.push(dc_heros)
+//marvel_heros.push(dc_heros)
 //Array koi bi data ly leta hai idr array array ko bi as a data single element ly ga 
 //output of this condition is 
 /*[ 'Thor', 'Ironman', 'Spiderman', [ 'Superman', 'Flash', 'Batman' ] ]
@@ -11,13 +11,13 @@ console.log(marvel_heros[3][1]);//3(which is array ) ki 1st value dy ga
            */           //2nd Method
 
 // /*///push work on existing array concate provide us new array
-/*const All_heros=marvel_heros.concat(dc_heros)
- console.log(All_heros);*/
+//const All_heros=marvel_heros.concat(dc_heros)
+//console.log(All_heros);
 //   /*                    //3rd Method
-// //spread operator
-// const all_new_heros=[...marvel_heros,...dc_heros]//Array got spread and every element become individual
+                        //spread operator
+ const all_new_heros = [...marvel_heros,...dc_heros]//Array got spread and every element become individual
 
-// console.log(all_new_heros);// most people use this ... add krky we can add another value
+ console.log(all_new_heros);// most people use this ... add krky we can add another value
 // */
 // /*                   //spreading all values of array
 // const another_array =[1,2,3,[4,5,6],7,[6,7,[4,5]]]
@@ -32,11 +32,11 @@ console.log(marvel_heros[3][1]);//3(which is array ) ki 1st value dy ga
 // console.log(Array.from({name:"Hammad"}));//will give me empty array  //interesting
 
 
-// let score1=100
-// let score2=200
-// let score3=300
+let score1=100
+let score2=200
+let score3=300
 
-// console.log(Array.of(score1,score2,score3))//return array from set of element
+console.log(Array.of(score1,score2,score3))//return array from set of element
 
 // //read more about array.is,array.from ,array.of
 
