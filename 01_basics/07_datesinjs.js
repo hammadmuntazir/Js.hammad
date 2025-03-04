626225661b3523c34df26b8db27195f8bf519468
hammadmuntazir <hammadmuntazir58@gmail.com>
2025-03-04 05:12:43 +0000

    datesinjs

--- 01_basics/07_datesinjs.js
+++ 01_basics/07_datesinjs.js
@@ -1,8 +1,8 @@
 // //Dates
 let myDate = new Date()
 // console.log(myDate.toString());
-// console.log(myDate.toLocaleDateString());
-// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());//will give me only date
+//console.log(myDate.toLocaleString());//will give me other time etc too
 // console.log(typeof myDate)//is an object
 
 //let myCreatedDate =new Date(2023, 0, 23)//month start with zero in JS
@@ -18,8 +18,8 @@ let myDate = new Date()
 //console.log(myCreatedDate.getTime());//now we can easily compare
 //console.log(Math.floor(Date.now()/1000));//for interview getting second
 
-// let newDate = new Date()
-// console.log(newDate);
+ //let newDate = new Date()
+ //console.log(newDate);
 // console.log(newDate.getMonth()+1);//month start from 0 so write +1
 // console.log(newDate.getDay());//day start from  monday and will get 3 on wednesday
 
