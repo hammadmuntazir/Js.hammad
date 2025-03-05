8eb246d7bf24bf8ca30dc703dfe621df4bd29880
hammadmuntazir <hammadmuntazir58@gmail.com>
2025-03-05 14:41:50 +0000

    iife.js

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,19 @@
+// Immediately Invoked Function Expressions(IIFE)
+        //namedIFFE,ParameterIFFE,2IFFE aik saat ,withoutname IFFE
+//()sy function ka aik block bana do
+(function chai(){
+    //namedIFFE
+    console.log(`DB CONNECTED`);
+})();
+//first parenthesis() jis mein jidr hum function ki defination likhny waly hain
+//()2nd wala execution
+/*=>Global Scope ky pollution sy problem hoti hai kafi baar
+to us global scope ky jo variable ya declaration ko hatanay ky liye 
+IIFE ka use krty hain*/
+
+//IFFE invoke to hogaya mgr usy pta nai hai context rokna kidr hai
+//usy end krny ky liye ;(semicolon use krna hoga)
+( (name)=>{
+    //without named IFFE
+    console.log(`DB CONNECTED TWO${name}`)
+})("hammad")//function ki trah parameter dy do 
