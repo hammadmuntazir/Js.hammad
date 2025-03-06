a9fe61f9fc9cbb4d34b1802b452d328215010494
hammadmuntazir <hammadmuntazir58@gmail.com>
2025-03-06 15:07:17 +0000

    three.js

---
+++ 05_iterations/three.js
@@ -0,0 +1,42 @@
+//for of
+//['',""]
+//[{},{},{}]
+const arr=[1,2,3,4,5]
+//for(const variable of jis cheeze pr loop lagana hai)
+for (const num of arr) {
+    console.log(num);
+    
+}
+
+const greetings="Hello World!"
+for(const greet of greetings){
+ //   console.log(`Each char is ${greet}`)
+ //   if(greet==" ")
+ //       break
+      if(greet==" ")
+        continue
+}
+
+   //Maps
+//map apna ap mein object hai//map remember orignal insertion order of key
+//object order ko yaad nai rakhty
+const map =new Map()
+map.set("IN","India")
+map.set("USA","United States of America")
+map.set("Pak","Pakistan")
+map.set("Fr","France")
+//again same value add kryu ga to double entry  nai hogi
+//value unique hogi aur jis order mein enter ki hai us order mein hogi
+map.set("Pak","Pakistan")
+//console.log(map);
+
+//assy pura sentex araha hai
+for(const key of map){
+//    console.log(key);
+}
+
+//key aur value dono ko separate print krny ky liye
+for (const[key,value]of map){
+    console.log(key,":",value);
+    
+}
