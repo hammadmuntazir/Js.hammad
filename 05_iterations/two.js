2b4b4e599d3a9c4896f4ae92c763b993d016e041
hammadmuntazir <hammadmuntazir58@gmail.com>
2025-03-06 13:14:26 +0000

    two.js

---
+++ 05_iterations/two.js
@@ -0,0 +1,34 @@
+
+let index=0;
+while(index<=10){
+//    console.log(`value of index is ${index}`);
+    index = index+2
+}
+let myArray=["Flash","Superman","Batman"]
+let arr=0
+while(arr<myArray.length){
+ //   console.log(`value is ${myArray[arr]}`)
+    arr=arr+1
+}
+
+/*       do while loop
+do {
+    
+} while (condition);
+ */
+
+let score=1
+do{
+//console.log(`Score is ${score}`)
+score++
+}while(score<=10);
+
+//kaam pahly hota hai condition baad mein check hoti hai
+//aik baar kaam krna hi hai haar haal mein ,bohat kum cases hoty hai usually
+//mostly pahly condition check krty hain
+let score1=11
+do{
+console.log(`Score is ${score1}`)
+score++
+}while(score1<=10);
+
